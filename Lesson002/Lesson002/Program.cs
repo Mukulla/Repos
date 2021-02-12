ce19b10637ad3b748614069434ebe47f061aaab2
Maxim <mbzozn@mail.ru>
2021-02-12 17:49:12 +0300

    Second Part
    Mked

--- Lesson002/Lesson002/Program.cs
+++ Lesson002/Lesson002/Program.cs
@@ -6,8 +6,13 @@ namespace Lesson002
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Часть первая - Средняя температура за сутки");
+            Console.WriteLine("Часть первая - Средняя температура за сутки");            
             CalculateAverageTempe();
+            Console.WriteLine();
+            
+            Console.WriteLine("Часть вторая - Название месяца по номеру");
+            NameMonth();
+            Console.WriteLine();
 
             Console.WriteLine();
             Console.WriteLine("Для завершения нажмите любую клавишу");
@@ -15,9 +20,11 @@ namespace Lesson002
         }
 
         public static void CalculateAverageTempe()
-        {
+        {            
             string Tempo;
             int TMin = 0, TMax = 0, TAverage = 0;
+            
+
             Console.WriteLine("Введите минимальную температуру за сутки ");
             TMin = GetNumberFromString();
 
@@ -47,6 +54,25 @@ namespace Lesson002
                 SomeValue = 0;
             }
             return SomeValue;
+        }
+
+        public static void NameMonth()
+        {
+            string[] Months = new string[] { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+            Console.WriteLine("Введите номер месяца ");
+            int ENM = GetNumberFromString();
+            --ENM;
+
+            if (ENM < 0)
+            {
+                ENM = 0;
+            }               
+            if (ENM > 11)
+            {
+                ENM = 11;
+            }
+
+            Console.WriteLine( Months[ ENM ] );
         }
     }
 }
