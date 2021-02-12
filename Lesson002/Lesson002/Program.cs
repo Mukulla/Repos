484ddee8562d32bd779f564a2d567f7c03f27dd4
Maxim <mbzozn@mail.ru>
2021-02-12 16:44:41 +0300

    Lesson002
    New solution created

---
+++ Lesson002/Lesson002/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
