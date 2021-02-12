a8493858cc8529ca11dcd5a38f81b72ea37a9b97
Maxim <mbzozn@mail.ru>
2021-02-12 17:37:11 +0300

    First Part
    Makd

--- Lesson002/Lesson002/Program.cs
+++ Lesson002/Lesson002/Program.cs
@@ -6,7 +6,47 @@ namespace Lesson002
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Часть первая - Средняя температура за сутки");
+            CalculateAverageTempe();
+
+            Console.WriteLine();
+            Console.WriteLine("Для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+
+        public static void CalculateAverageTempe()
+        {
+            string Tempo;
+            int TMin = 0, TMax = 0, TAverage = 0;
+            Console.WriteLine("Введите минимальную температуру за сутки ");
+            TMin = GetNumberFromString();
+
+            Console.WriteLine("Введите максимальную температуру за сутки ");
+            TMax = GetNumberFromString();
+
+            if( TMin > TMax)
+            {
+                int Swapperou = TMin;
+                TMin = TMax;
+                TMax = Swapperou;
+            }
+
+            TAverage = ( TMin + TMax ) / 2;
+            Console.WriteLine( "Средняя температура за день равна: " + TAverage );
+        }
+
+        public static int GetNumberFromString()
+        {
+            string TempoString;
+            int SomeValue;
+            //Получаем символы в строке
+            TempoString = Console.ReadLine();
+            //Проверяем на соответствие типу int
+            if( !int.TryParse(TempoString, out SomeValue) )
+            {
+                SomeValue = 0;
+            }
+            return SomeValue;
         }
     }
 }
