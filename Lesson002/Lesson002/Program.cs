9b5c488cde48a6e5fc358e697da14b8a65e44f8b
Maxim <mbzozn@mail.ru>
2021-02-12 18:08:31 +0300

    Part Four
    Mkd

--- Lesson002/Lesson002/Program.cs
+++ Lesson002/Lesson002/Program.cs
@@ -5,25 +5,37 @@ namespace Lesson002
     class Program
     {
         static void Main(string[] args)
-        {/*
+        {
+            int Avaragou, Monthou;
+            
             Console.WriteLine("Часть первая - Средняя температура за сутки");            
-            CalculateAverageTempe();
+            Avaragou = CalculateAverageTempe();
             Console.WriteLine();
             
             Console.WriteLine("Часть вторая - Название месяца по номеру");
-            NameMonth();
+            Monthou = NameMonth();
             Console.WriteLine();
-            */
+            
             Console.WriteLine("Часть третья - Определение чётности числа");
             DetermineTheNumber();
             Console.WriteLine();
+
+            
+            if (Avaragou > 0)
+            {
+                if (Monthou > -1 && Monthou < 1 || Monthou == 11)
+                {
+                    Console.WriteLine("Часть четвёртая - Итог по температуре и месяцам");
+                    Console.WriteLine("Дождливая зима");
+                }
+            }
 
             Console.WriteLine();
             Console.WriteLine("Для завершения нажмите любую клавишу");
             Console.ReadKey();
         }
 
-        public static void CalculateAverageTempe()
+        public static int CalculateAverageTempe()
         {            
             string Tempo;
             int TMin = 0, TMax = 0, TAverage = 0;
@@ -44,6 +56,7 @@ namespace Lesson002
 
             TAverage = ( TMin + TMax ) / 2;
             Console.WriteLine( "Средняя температура за день равна: " + TAverage );
+            return TAverage;
         }
 
         public static int GetNumberFromString()
@@ -60,7 +73,7 @@ namespace Lesson002
             return SomeValue;
         }
 
-        public static void NameMonth()
+        public static int NameMonth()
         {
             string[] Months = new string[] { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
             Console.WriteLine("Введите номер месяца ");
@@ -77,6 +90,7 @@ namespace Lesson002
             }
 
             Console.WriteLine( Months[ ENM ] );
+            return ENM;
         }
 
         public static void DetermineTheNumber()
@@ -90,6 +104,11 @@ namespace Lesson002
                 return;
             }
             Console.WriteLine("Нечётное");
+        }
+
+        public static void Fourero()
+        {
+
         }
     }
 }
